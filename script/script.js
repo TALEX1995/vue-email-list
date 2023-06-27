18ce91a1231cbfe2133ce4d245f9828a7c87cfe6
TALEX1995 <tanase.alex@libero.it>
2023-06-27 15:36:30 +0200

    Added get random Email

--- script/script.js
+++ script/script.js
@@ -2,7 +2,28 @@ console.log('JS OK', Vue)
 
 // Initialize app
 const app = Vue.createApp ({
+    data: () => {
+        return {
+            eMail: [],
+            eMailNumber: 10
+        }
+    },
 
+    methods: {
+        getRandomEmail () {
+            for(let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                        const randomEmail = res.data.response
+                        this.eMail.push(randomEmail)
+                    })
+            }
+        }
+    },
+
+    created () {
+        this.getRandomEmail()
+    }
 })
 
 
